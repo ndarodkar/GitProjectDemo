a2d5c33bb7ea541c25fa3d02b2abcd300956cd17
-neha- <-neha.darodkar@gmail.com>
2022-03-30 12:46:06 -0500

    My first commit

---
+++ src/test/java/MavenJava/MavenJava/AppiumTest.java
@@ -0,0 +1,15 @@
+package MavenJava.MavenJava;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void AppiumFirst()
+	{
+		System.out.println("This is my first Appium test");
+		
+	}
+
+}
---
+++ src/test/java/MavenJava/MavenJava/SeleniumTest.java
@@ -0,0 +1,15 @@
+package MavenJava.MavenJava;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+    @Test
+	public void SeleniumFirst()
+	{
+		
+		System.out.println("Executed first test of Selenium");
+	}
+
+}
